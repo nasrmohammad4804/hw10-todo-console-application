fde0fa6cd5e95460bc49a11ce4cbeec20acf8849
mohammad nasr <nasrmohammad4804@gmail.com>
2021-08-10 15:13:36 +0430

    last update

--- project/src/test/java/Main.java
+++ project/src/test/java/Main.java
@@ -1,5 +1,3 @@
-import java.util.LinkedHashMap;
-
 public class Main {
 
     public static void main(String[] args) {
@@ -44,24 +42,28 @@ public class Main {
 
         System.out.println("##################################################");
 
-        MyLinkedList.LRUCache<Integer,String> lruCache=new MyLinkedList.LRUCache<>();
+        MyLinkedList.LRUCache<Integer, String> lruCache = new MyLinkedList.LRUCache<>();
 
         System.out.println(lruCache.get(12));
 
-        lruCache.put(15,"mohammad");
-        lruCache.put(11,"javad");
-        lruCache.put(13,"aida");
-        lruCache.put(26,"reza");
+        lruCache.put(15, "mohammad");
+        lruCache.put(11, "javad");
+        lruCache.put(13, "aida");
+        lruCache.put(26, "reza");
 
         System.out.println(lruCache);
 
-        System.out.println(lruCache.put(11,"zahra"));
+        System.out.println(lruCache.put(11, "zahra"));
         System.out.println(lruCache);
 
-        System.out.println(lruCache.put(34,"farshid"));
+        System.out.println(lruCache.put(34, "farshid"));
         System.out.println(lruCache);
 
-        System.out.println(lruCache.put(15,"masoud"));
+        System.out.println(lruCache.put(15, "masoud"));
+
+        System.out.println(lruCache);
+
+        System.out.println(lruCache.put(99, "kazem"));
 
         System.out.println(lruCache);
 
