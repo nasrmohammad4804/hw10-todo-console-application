9a339669af89cb0759faf44469c56a0e16acdea7
mohammad nasr <nasrmohammad4804@gmail.com>
2021-08-14 14:04:33 +0430

    add number to my enum for check if already set In_Progress now we must to choice Completed

--- project/src/main/java/domain/enumaration/StateOfActivity.java
+++ project/src/main/java/domain/enumaration/StateOfActivity.java
@@ -3,7 +3,19 @@ package domain.enumaration;
 public enum StateOfActivity {
 
 
-    OPEN,IN_PROGRESS,COMPLETED;
+    OPEN(1),IN_PROGRESS(2),COMPLETED(3);
+
+    private  int number;
+
+
+    StateOfActivity(int number){
+
+        this.number =number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
 
     @Override
     public String toString() {
