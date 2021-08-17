af94024ce679a2f42c606ae690e9b126c31ef05a
mohammad nasr <nasrmohammad4804@gmail.com>
2021-08-17 11:13:25 +0430

    add interface UserService & ActivityService for declare of duty service

--- project/src/main/java/service/ActivityService.java
+++ project/src/main/java/service/ActivityService.java
@@ -1,119 +1,16 @@
 package service;
 
-import domain.Activities;
+import domain.Activity;
 import domain.User;
-import domain.enumaration.StateOfActivity;
-import repository.ActivityRepo;
-import service.impl.BaseServiceImpl;
 
-import java.util.Arrays;
-import java.util.Comparator;
 import java.util.List;
-import java.util.Scanner;
-import java.util.stream.Collectors;
 
-public class ActivityService extends BaseServiceImpl<Activities, ActivityRepo> {
+public interface ActivityService {
 
-    private final Scanner scannerForString = new Scanner(System.in);
-    private final Scanner scannerForInteger = new Scanner(System.in);
+    void showActivity(List<Activity> activities);
 
-    protected ActivityService(ActivityRepo repository) {
-        super(repository);
+    void updateActivity(User user);
 
-    }
-
-    public void showActivity(List<Activities> activities) {
-
-        if (activities.isEmpty()) {
-            System.out.println("dont activity yet\n\n");
-            return;
-        }
-
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("if you want show Ascending enter asc \nif you want Descending enter desc ");
-        String result = scanner.nextLine();
-
-        switch (result) {
-
-            case "asc":
-                showAscendingActivity(activities);
-                break;
-
-            case "desc":
-                showDescendingActivity(activities);
-                break;
-            default:
-                System.out.println("your input not valid try again");
-                showActivity(activities);
-        }
-
-    }
-
-    private void showDescendingActivity(List<Activities> activities) {
-
-        System.out.printf("%50s\n", "***************|All Activity|***************");
-        activities.stream().sorted(Comparator.reverseOrder()).forEach(System.out::println);
-
-        System.out.printf("%48s \n","------------------------------------------");
-    }
-
-    private void showAscendingActivity(List<Activities> activities) {
-
-        System.out.printf("%50s\n", "***************|All Activity|***************");
-        activities.stream().sorted().forEach(System.out::println);
-
-        System.out.printf("%48s \n","------------------------------------------");
-    }
-
-    public void addActivity(Activities activities) {  // move to userService
-
-
-        repository.add(activities);
-
-    }
-
-    public void changeStatus(User user) {
-
-        List<Activities> list = repository.getAllActivity(user);
-
-        if (list.isEmpty()) {
-            System.out.println("not activity for user\n\n");
-            return;
-        }
-
-        for (Activities activities : list)
-            System.out.println(activities);
-
-        System.out.println("enter activity");
-        int number = scannerForInteger.nextInt();
-
-        try {
-
-            Activities activities = list.stream().filter(x -> x.getId() == number).findFirst().get();
-            System.out.println("status of this activity is : "+activities.getStateOfActivity()+"\n");
-
-            System.out.print("enter status from  ");
-          List<StateOfActivity> myList =  Arrays.stream(StateOfActivity.values()).filter(x -> x.getNumber()>activities.
-                    getStateOfActivity().getNumber()).collect(Collectors.toList());
-
-            System.out.println(myList);
-
-            StateOfActivity state = StateOfActivity.valueOf(scannerForString.nextLine());
-
-            if (state.getNumber() > activities.getStateOfActivity().getNumber()) {
-
-                activities.setStateOfActivity(state);
-                System.out.println("state change to " + state + "\n\n");
-            } else System.out.println("not possible to change status -_- ..");
-
-        } catch (Exception e) {
-            System.out.println("this activityNumber not exists !!!");
-        }
-
-    }
-
-    public List<Activities> getAllActivity(User user) {
-
-        return repository.getAllActivity(user);
-    }
+    List<Activity> getAllActivity(User user);
 }
+
--- project/src/main/java/service/UserService.java
+++ project/src/main/java/service/UserService.java
@@ -1,132 +1,17 @@
 package service;
 
-import domain.Activities;
+import base.service.BaseService;
 import domain.User;
-import domain.enumaration.StateOfActivity;
-import org.hibernate.Session;
-import repository.UserRepo;
-import service.impl.BaseServiceImpl;
-import util.HibernateUtil;
 
-import java.sql.Time;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Scanner;
-import java.util.concurrent.atomic.AtomicReference;
+public interface UserService extends BaseService<User,Long> {
 
-public class UserService extends BaseServiceImpl<User, UserRepo> {
+    User login();
 
-    private final Scanner scannerForString = new Scanner(System.in);
+    User register();
 
-    private final ActivityService activityService;
+    void showActivity(User user);
 
+    void addActivity(User user);
 
-    protected UserService(UserRepo repository, ActivityService activityService) {
-        super(repository);
-        this.activityService = activityService;
-    }
-
-    public User login() {
-
-        System.out.println("enter userName");
-        String userName = scannerForString.nextLine();
-
-        System.out.println("enter password");
-        String password = scannerForString.nextLine();
-
-        List<User> list = repository.getAll();
-
-        for (User user : list)
-            if (user.getUserName().equals(userName))
-                if (user.getPassword().equals(password))
-                    return user;
-
-        return null;
-    }
-
-    public User register() {
-
-        System.out.println("enter firstName");
-
-        String firstName = scannerForString.nextLine();
-
-        System.out.println("enter family");
-
-        String family = scannerForString.nextLine();
-
-        System.out.println("enter nationalCode");
-
-        String nationalCode = scannerForString.nextLine();
-
-        System.out.println("enter userName");
-        String userName = scannerForString.nextLine();
-
-        System.out.println("enter password");
-        String password = scannerForString.nextLine();
-
-        AtomicReference<User> user = null;
-
-        repository.getAll().forEach(x -> {
-            if (x.getUserName().equals(userName) && x.getPassword().equals(password)) {
-                assert false;
-                user.set(x);
-            }
-        });
-
-        try {
-            assert false;
-            if (user.get() != null)
-                return null;
-
-
-        } catch (Exception e) {
-
-            User myUser = new User(firstName, family, nationalCode);
-            myUser.setUserName(userName);
-            myUser.setPassword(password);
-
-            Session session = HibernateUtil.getSESSION();
-            session.getTransaction().begin();
-            repository.add(myUser);
-
-            session.getTransaction().commit();
-            return myUser;
-        }
-
-        return null;
-
-    }
-
-    public void showActivity(User user) {
-
-        for (User us : repository.getAll())
-            if (user.equals(us))
-                activityService.showActivity(user.getActivities());
-    }
-
-    public void addActivity(User user) {
-
-
-        System.out.println("enter your task ..");
-        String task = scannerForString.nextLine();
-
-        System.out.println("enter time ");
-
-        Time time = Time.valueOf(scannerForString.nextLine());
-
-
-        Activities activity = new Activities(task, time, StateOfActivity.OPEN);
-
-        Session session = HibernateUtil.getSESSION();
-
-        session.getTransaction().begin();
-
-        user.getActivities().add(activity);
-
-        repository.add(user);
-        activityService.addActivity(activity);
-        session.getTransaction().commit();
-
-    }
 
 }
